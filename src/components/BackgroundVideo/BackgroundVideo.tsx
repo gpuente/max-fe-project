82f4ac0babb379575d04cced7c920918732f0ae7
Guillermo Puente <guillermo.ps09@gmail.com>
2023-08-15 10:38:10 -0400

    🐞 fix: styles bg video in mobile devices

--- src/components/BackgroundVideo/BackgroundVideo.tsx
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -1,10 +1,12 @@
 import { Container } from './styles';
 import video from '../../assets/bg-video.mp4';
+import image from '../../assets/bg-image.jpg';
 
 export const BackgroundVideo: React.FC = () => (
   <Container>
-    <video autoPlay muted loop>
+    <video autoPlay muted loop playsInline>
       <source src={video} type="video/mp4" />
+      <img src={image} alt="background" />
     </video>
   </Container>
 );
--- src/components/BackgroundVideo/styles.ts
+++ src/components/BackgroundVideo/styles.ts
@@ -7,7 +7,7 @@ export const Container = styled('div')`
   width: 100%;
   height: 100%;
 
-  & > video {
+  & > video, & > img  {
     width: 100%;
     height: 100%;
     object-fit: cover;
